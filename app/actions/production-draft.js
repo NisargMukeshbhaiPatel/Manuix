bb461357e8f9c5597d36442bfda8c5e968fbeeb6
NisargMukeshbhaiPatel <nishupatel2550@icloud.com>
2025-09-10 04:11:34 +1000

    Seperate server/client page for prod-drafts & "use server" for it.

--- app/actions/production-draft.js
+++ app/actions/production-draft.js
@@ -1,6 +1,9 @@
+"use server"
+import dbConnect from "@/lib/db";
 import ProductionDraft from "@/models/ProductionDraft";
 
 export async function getProductionDrafts() {
+  await dbConnect();
   // Get all drafts, populate product and shortages
   return await ProductionDraft.find({})
     .populate("product_id")
